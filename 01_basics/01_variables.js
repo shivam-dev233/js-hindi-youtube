8bead8d931aa46b73d6ee3ba4209fe5572f045bd
Shivam ray <ray077452@gmail.com>
2025-06-18 10:00:50 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = "92295498104"
+let accountEmail = "ray077452@gmail.com"
+var accountPassword = "13345"
+accountCity = "patna"
+
+// account_Id = 2
+
+accountEmail = "ray@0452"
+accountPassword = "32145"
+accountCity = "mohania"
+
+console.table([accountEmail, accountId, accountPassword, accountCity])

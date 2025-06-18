0ee72508d3863ea198a22fc90e8c7a314d7ad8cb
Shivam ray <ray077452@gmail.com>
2025-06-18 10:41:37 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,31 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("shivam")
+
+
+
+let name = "Shivam"
+let age = 18
+let isloggedIn = "false"
+
+
+/*Primitive datatype:-
+
+1.number => 2 to power 53
+2.bigint
+3.string => ""
+4.boolean => true/false
+5.null => standalone value
+6.under => 
+7.symbol =>unique
+*/
+
+/* Object
+
+console.log(typeof 2); => number
+console.log(typeof null); => object
+console.log(typeof undefined); => undifined
+*/

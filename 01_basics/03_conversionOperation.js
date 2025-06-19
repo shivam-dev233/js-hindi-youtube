f0b323c94d52ea9f96be610f8b31617d26cad4a3
Shivam ray <ray077452@gmail.com>
2025-06-19 07:12:40 +0000

    con

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = true
+
+// console.log(typeof  score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof  valueInNumbe);
+
+//  1 => true ;
+//  0 => false ;
+//  "" => false ;
+
+
+
